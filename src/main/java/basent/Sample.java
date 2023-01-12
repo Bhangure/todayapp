f1651c00a9073c76217a60df563a7cfbb9b26e92
Bhangure <112766432+Bhangure@users.noreply.github.com>
2023-01-12 08:50:14 +0530

    Update Sample.java

--- src/main/java/basent/Sample.java
+++ src/main/java/basent/Sample.java
@@ -3,5 +3,6 @@ package basent;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("this is my second app");
     }
 }
